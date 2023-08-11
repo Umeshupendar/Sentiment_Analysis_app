8bab655ad1a72e54072263852537468b917f4181
umesh <umeshchellasamy2525@gmail.com>
2023-08-12 01:23:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form['text']
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(text)['compound']
+
+        if score > 0:
+            label = 'Positive'
+        elif score == 0:
+            label = 'Neutral'
+        else:
+            label = 'Negative'
+
+        return render_template("index.html", text=text, label=label)
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
